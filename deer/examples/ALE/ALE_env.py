dcaecde246f1704838a5a63bd8ccce81003bf36d
Axel Foltyn <axel.foltyn@gmail.com>
2022-03-16 09:33:41 +0100

    add deer from VinF github

---
+++ deer/examples/ALE/ALE_env.py
@@ -0,0 +1,104 @@
+""" Interface with the ALE environment
+
+"""
+
+import numpy as np
+import cv2
+from ale_python_interface import ALEInterface
+from deer.base_classes import Environment
+
+from mpl_toolkits.axes_grid1 import host_subplot
+import mpl_toolkits.axisartist as AA
+import matplotlib.pyplot as plt
+
+class MyEnv(Environment):
+    VALIDATION_MODE = 0
+
+    def __init__(self, rng, rom="ale/breakout.bin", frame_skip=4, 
+                 ale_options=[{"key": "random_seed", "value": 0}, 
+                              {"key": "color_averaging", "value": True},
+                              {"key": "repeat_action_probability", "value": 0.}]):
+        self._mode = -1
+        self._mode_score = 0.0
+        self._mode_episode_count = 0
+
+        self._frame_skip = frame_skip if frame_skip >= 1 else 1
+        self._random_state = rng
+
+        self._ale = ALEInterface()
+        for option in ale_options:
+            t = type(option["value"])
+            if t is int:
+                self._ale.setInt(option["key"], option["value"])
+            elif t is float:
+                self._ale.setFloat(option["key"], option["value"])
+            elif t is bool:
+                self._ale.setBool(option["key"], option["value"])
+            else:
+                raise ValueError("Option {} ({}) is not an int, bool or float.".format(option["key"], t))
+        self._ale.loadROM(rom)
+
+        w, h = self._ale.getScreenDims()
+        self._screen = np.empty((h, w), dtype=np.uint8)
+        self._reduced_screen = np.empty((84, 84), dtype=np.uint8)
+        self._actions = self._ale.getMinimalActionSet()
+
+                
+    def reset(self, mode):
+        if mode == MyEnv.VALIDATION_MODE:
+            if self._mode != MyEnv.VALIDATION_MODE:
+                self._mode = MyEnv.VALIDATION_MODE
+                self._mode_score = 0.0
+                self._mode_episode_count = 0
+            else:
+                self._mode_episode_count += 1
+        elif self._mode != -1: # and thus mode == -1
+            self._mode = -1
+
+        self._ale.reset_game()
+        for _ in range(self._random_state.randint(15)):
+            self._ale.act(0)
+        self._ale.getScreenGrayscale(self._screen)
+        cv2.resize(self._screen, (84, 84), self._reduced_screen, interpolation=cv2.INTER_NEAREST)
+        
+        return [4 * [84 * [84 * [0]]]]
+        
+        
+    def act(self, action):
+        action = self._actions[action]
+        
+        reward = self._ale.act(action)
+        #if self.inTerminalState():
+        #    break
+            
+        self._ale.getScreenGrayscale(self._screen)
+        cv2.resize(self._screen, (84, 84), self._reduced_screen, interpolation=cv2.INTER_NEAREST)
+  
+        self._mode_score += reward
+        return np.sign(reward)
+
+    def summarizePerformance(self, test_data_set, *args, **kwargs):
+        if self.inTerminalState() == False:
+            self._mode_episode_count += 1
+        print("== Mean score per episode is {} over {} episodes ==".format(self._mode_score / self._mode_episode_count, self._mode_episode_count))
+
+
+    def inputDimensions(self):
+        return [(4, 84, 84)]
+
+    def observationType(self, subject):
+        return np.uint8
+
+    def nActions(self):
+        return len(self._actions)
+
+    def observe(self):
+        return [np.array(self._reduced_screen)]
+
+    def inTerminalState(self):
+        return self._ale.game_over()
+                
+
+
+if __name__ == "__main__":
+    pass
---
+++ deer/examples/gym/mountain_car_continuous_env.py
@@ -0,0 +1,110 @@
+""" Mountain car environment with continuous action space.
+
+Author: Vincent Francois-Lavet
+"""
+
+import numpy as np
+import copy
+import math
+from deer.base_classes import Environment
+import gym
+
+class MyEnv(Environment):
+    def __init__(self, rng):
+        """ Initialize environment.
+
+        Parameters
+        -----------
+            rng : numpy random number generator
+        """
+        self.env = gym.make('MountainCarContinuous-v0')
+        self.rng=rng
+        self._last_observation = self.reset()
+        self.is_terminal=False
+        self._input_dim = [(1,), (1,)]
+        
+    def act(self, action):
+        """ Simulate one time step in the environment and returns the reward for the time step
+        
+        Parameters
+        -----------
+        action : list of floats (in this case one float, because there is one action)
+
+        Returns
+        -------
+        reward : float
+        """
+        reward=0
+        for _ in range(10): # Increase the duration of one time step by a factor 10
+            self._last_observation, r, self.is_terminal, info = self.env.step([action[0]])
+            reward+=r
+            if(self.is_terminal==True):
+                break
+                
+            if (self.mode==0): # Show the policy only at test time
+                try:
+                    self.env.render()
+                except:
+                    pass
+                
+        return reward/100. #Scale the reward so that it's 1 at maximum
+                
+    def reset(self, mode=0):
+        """ Reset environment for a new episode.
+
+        Parameters
+        -----------
+        Mode : int
+            -1 corresponds to training and 0 to test
+        """
+        self.mode=mode
+        
+        self._last_observation = self.env.reset()
+
+        self.is_terminal=False
+
+        return self._last_observation
+                
+    def inTerminalState(self):
+        """ This returns whether the environment reached a terminal state after the last transition 
+        (i.e. whether the last transition that occurred was terminal).
+
+        Returns
+        -------
+        self.is_terminal : bool
+        """
+        return self.is_terminal
+
+    def inputDimensions(self):
+        return self._input_dim  
+
+    def nActions(self):
+        """ Provides the bounds on the action space
+
+        Returns
+        -------
+        bounds on the action space
+        """
+        return [[self.env.action_space.low[0],self.env.action_space.high[0]]]
+
+    def observe(self):
+        return copy.deepcopy(self._last_observation)
+        
+def main():
+    # This function can be used for debug purposes
+    rng = np.random.RandomState(123456)
+    myenv=MyEnv(rng)
+    print(myenv.env.action_space)
+    print(myenv.env.action_space.low)
+    print(myenv.env.action_space.high)    
+    print(myenv.env.observation_space)
+    
+    print (myenv.observe())
+    myenv.act([0])
+    print (myenv.observe())
+    myenv.act([1])
+    print (myenv.observe())
+    
+    
+if __name__ == "__main__":
+    main()
